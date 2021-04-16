7af90085e951b4fd302ba832df1eeb828c557db7
Kevin Perez Sesma <a20311111@uthermosillo.edu.mx>
2021-04-16 14:30:59 -0700

    Nueva rama y creación y código de clases User, Cashier, Admin
    Signed-off-by: Kevin Perez Sesma <a20311111@uthermosillo.edu.mx>

---
+++ PDV2021/code/PdvUth2021/LibPdvUth2021/Users/Cashier.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibPdvUth2021.Users
+{
+    class Cashier
+    {
+    }
+}
---
+++ PDV2021/code/PdvUth2021/LibPdvUth2021/Users/User.cs
@@ -0,0 +1,32 @@
+﻿using LibPdvUth2021.App;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibPdvUth2021.Users
+{
+    public class User : CRUD
+    {
+        //contructor
+        public User() : base("users", new List<string>() { "id", "name", "lastname", "mothers_lastname", "email", "password", "type", "street", "house_number", "residential", "postal_code", "region_state", "CURP", "RFC" })
+        {
+
+        }
+        //attribs
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public double MothersLastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int UserType { get; set; }
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public string Residential { get; set; }
+        public string PostalCode { get; set; }
+        public string RegionState { get; set; }
+        public string CURP { get; set; }
+        public string RFC { get; set; }
+
+    }
+}
